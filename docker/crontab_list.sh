7d44c1cebc015750cae7a853b938a6b3fcf6bf4d
小朋 <gossh@meantinc.com>
2021-06-27 13:36:37 +0800

    Create crontab_list.sh

---
+++ docker/crontab_list.sh
@@ -0,0 +1,15 @@
+# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
+50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
+#收集助力码
+30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
+
+##############短期活动##############
+#京东极速版红包(活动时间：2021-5-5至2021-5-31)
+45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
+##############长期活动##############
+# 签到
+7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
+# 京东抽奖机
+0 0,12,23 * * * node /scripts/jd_lotteryMachine.js >> /scripts/logs/jd_lotteryMachine.log 2>&1
+# 京东排行榜
+21 9 * * * node /scripts/jd_rankingList.js >> /scripts/logs/jd_rankingList.log 2>&1

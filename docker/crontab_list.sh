ad297f01632a4d9fe5acd1ef737391e7c051c5ca
小朋 <gossh@meantinc.com>
2021-07-09 02:31:34 +0800

    ss

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,6 +6,8 @@
 ##############短期活动##############
 #点点券二代目
 45 0,23 * * * node /scripts/jd_necklace_new.js >> /scripts/logs/jd_necklace_new.log 2>&1
+#燃动夏季7.8-8.8
+12 6-23/2 * * * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
@@ -15,3 +17,5 @@
 21 9 * * * node /scripts/jd_rankingList.js >> /scripts/logs/jd_rankingList.log 2>&1
 #宠汪汪二代目
 45 */2,23 * * * node /scripts/jd_joy_new.js >> /scripts/logs/jd_joy_new.log 2>&1
+#京东签到图形验证
+14 10 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1

47d95eafd7f0654e3c122a48481ae3e47fe02c84
GitHub Action <action@github.com>
2021-07-10 16:00:11 +0000

    avoid being suspended

---
+++ ZooFaker_Necklace.js
@@ -0,0 +1,1 @@
+<a href="https://endpoint.fastgit.org/https://raw.githubusercontent.com/smiek2221/scripts/master/ZooFaker_Necklace.js">Moved Permanently</a>.
---
+++ jd_necklace.js
@@ -0,0 +1,1 @@
+<a href="https://endpoint.fastgit.org/https://raw.githubusercontent.com/smiek2221/scripts/master/jd_necklace.js">Moved Permanently</a>.

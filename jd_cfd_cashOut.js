b4365b445acab409c7554133acbe26c1cf851bda
GitHub Action <action@github.com>
2021-07-20 18:20:29 +0000

    .更新

--- jd_cfd_cashOut.js
+++ jd_cfd_cashOut.js
@@ -1,5 +1,6 @@
 "use strict";
 /**
+ * 0～30秒开始执行，31～59秒死循环等待
  * 提现金额：0.1、0.5、1、2、10
  * 解锁提现方式：升级1个建筑，留金币够升级一个建筑
  * 自动模拟提现token，不需要抓包
@@ -64,43 +65,54 @@ var cookie = '', res = '', UserName, index;
                 i = 0;
                 _b.label = 3;
             case 3:
-                if (!(i < cookiesArr.length)) return [3 /*break*/, 14];
+                if (!(i < cookiesArr.length)) return [3 /*break*/, 19];
                 cookie = cookiesArr[i];
                 UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
                 index = i + 1;
                 console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7" + index + "\u3011" + UserName + "\n");
-                _i = 0, _a = ['food', 'fun', 'shop', 'sea'];
                 _b.label = 4;
             case 4:
-                if (!(_i < _a.length)) return [3 /*break*/, 8];
+                if (!1) return [3 /*break*/, 8];
+                if (!(new Date().getSeconds() < 30)) return [3 /*break*/, 5];
+                return [3 /*break*/, 8];
+            case 5: return [4 /*yield*/, TS_USER_AGENTS_1.wait(100)];
+            case 6:
+                _b.sent();
+                _b.label = 7;
+            case 7: return [3 /*break*/, 4];
+            case 8:
+                _i = 0, _a = ['food', 'fun', 'shop', 'sea'];
+                _b.label = 9;
+            case 9:
+                if (!(_i < _a.length)) return [3 /*break*/, 13];
                 b = _a[_i];
                 return [4 /*yield*/, api('user/GetBuildInfo', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strBuildIndex,strZone', { strBuildIndex: b })];
-            case 5:
+            case 10:
                 res = _b.sent();
-                if (!(res.dwCanLvlUp === 1)) return [3 /*break*/, 7];
+                if (!(res.dwCanLvlUp === 1)) return [3 /*break*/, 12];
                 return [4 /*yield*/, api('user/BuildLvlUp', '_cfd_t,bizCode,ddwCostCoin,dwEnv,ptag,source,strBuildIndex,strZone', { ddwCostCoin: res.ddwNextLvlCostCoin, strBuildIndex: b })];
-            case 6:
+            case 11:
                 res = _b.sent();
                 if (res.iRet === 0) {
                     console.log("\u5347\u7EA7\u6210\u529F:", res); // ddwSendRichValue
-                    return [3 /*break*/, 8];
+                    return [3 /*break*/, 13];
                 }
-                _b.label = 7;
-            case 7:
+                _b.label = 12;
+            case 12:
                 _i++;
-                return [3 /*break*/, 4];
-            case 8:
+                return [3 /*break*/, 9];
+            case 13:
                 // 提现
                 console.log('解锁：', date_fns_1.format(new Date(), 'hh:mm:ss:SSS'));
                 return [4 /*yield*/, getJxToken(cookie)];
-            case 9:
+            case 14:
                 token_1 = _b.sent();
                 return [4 /*yield*/, api('user/CashOutQuali', '_cfd_t,bizCode,dwEnv,ptag,source,strPgUUNum,strPgtimestamp,strPhoneID,strZone', { strPgUUNum: token_1.strPgUUNum, strPgtimestamp: token_1.strPgtimestamp, strPhoneID: token_1.strPhoneID })];
-            case 10:
+            case 15:
                 res = _b.sent();
                 console.log('资格:', res);
                 return [4 /*yield*/, TS_USER_AGENTS_1.wait(2000)];
-            case 11:
+            case 16:
                 _b.sent();
                 console.log('提现：', date_fns_1.format(new Date(), 'hh:mm:ss:SSS'));
                 money = 10;
@@ -119,14 +131,14 @@ var cookie = '', res = '', UserName, index;
                 money = process.env.CFD_CASHOUT_MONEY ? parseFloat(process.env.CFD_CASHOUT_MONEY) * 100 : money;
                 console.log('本次计划提现：', money);
                 return [4 /*yield*/, api('user/CashOut', '_cfd_t,bizCode,ddwMoney,ddwPaperMoney,dwEnv,ptag,source,strPgUUNum,strPgtimestamp,strPhoneID,strZone', { ddwMoney: money, ddwPaperMoney: money * 10, strPgUUNum: token_1.strPgUUNum, strPgtimestamp: token_1.strPgtimestamp, strPhoneID: token_1.strPhoneID })];
-            case 12:
+            case 17:
                 res = _b.sent();
                 console.log('提现:', res);
-                _b.label = 13;
-            case 13:
+                _b.label = 18;
+            case 18:
                 i++;
                 return [3 /*break*/, 3];
-            case 14: return [2 /*return*/];
+            case 19: return [2 /*return*/];
         }
     });
 }); })();
